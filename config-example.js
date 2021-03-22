f882f21bdaded05539a27fcd5290a52dcc3887aa
eag1ex <eaglex@eaglex.net>
2021-03-22 08:33:24 +0000

    removed config

--- config-example.js
+++ config-example.js
@@ -5,15 +5,16 @@
 
 const port = process.env.PORT || (process.argv[2] || 5000)
 const dbRemote = false
+const path = require('path')
 
 module.exports = {
     // in production the authentication is enabled
     env: 'development', // development,production
-
-    port: (typeof port === 'number') ? port : 5000,
+    port: port,
     'secret': '', // NOTE provide your static token
     HOST: `http://localhost:${port}`, //  in localhost keep this line
     // HOST: !dbRemote ? `http://localhost:${port}` : `www`,
+    viewsDir: path.join(__dirname, './views'),
     mongo: {
         remote: dbRemote,
         database: dbRemote ? `mongodb://localhost/bucket_list` : `mongodb://localhost/bucket_list`,
